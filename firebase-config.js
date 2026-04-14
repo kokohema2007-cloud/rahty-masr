54629c9f990818cc56456042151412b1bbe5d8ca
Have fun <kokohema2007@gmail.com>
2026-04-14 19:46:03 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase v10+ config - can be imported or used globally
+export const firebaseConfig = {
+  apiKey: "AIzaSyATX4lg9dMzxQy98YBX6OR-DSx9foje32U",
+  authDomain: "rahty-masr.firebaseapp.com",
+  projectId: "rahty-masr",
+  storageBucket: "rahty-masr.firebasestorage.app",
+  messagingSenderId: "256669602229",
+  appId: "1:256669602229:web:dc5c4de8aadc67f26609fc",
+  measurementId: "G-WYEXNE8PF1"
+};
+
